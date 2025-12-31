f59d2c05e89b0ed2f91fb7e10ec8d33eca9b19d4
dongdong7-max <dongdong7@handong.ac.kr>
2025-12-31 14:17:08 +0900

    feat: env.example 파일 업로드, database.py 수정

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlmodel import create_engine, Session
 
 # 1. DB 접속 정보 설정 (이미지에서 설정한 정보와 일치해야 함)
 # 형식: postgresql://사용자:비밀번호@호스트:포트/DB이름
-DATABASE_URL = "postgresql://hajunkim:1q2w3e4r@127.0.0.1:5432/gold_db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 # 2. 엔진 생성
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True는 SQL 로그를 터미널에 보여
 
